3238b5ec0016a5d886657c17b749752e39c9c6c5
arvindh <arvindhganesh11@gmail.com>
2026-03-26 09:40:33 +0530

    add sync and async code

---
+++ Season-01/07-Sync-Async/async.js
@@ -0,0 +1,24 @@
+const https = require("https");
+const fs = require("fs");
+
+let a = 85148;
+let b = 21885;
+
+setTimeout(() => {
+    console.log("Calling set tiemmout after 5 seconds");
+}, 5000);
+
+https.get("https://dummyjson.com/test", (res) => {
+    res.resume();
+    console.log("Fetched Data");
+})
+
+fs.readFile("./file.txt", "utf-8", (err, data) => {
+    console.log("Data read successfully ", data);
+})
+
+function multiply(a, b) {
+    return a * b;
+}
+let c = multiply(a, b);
+console.log("Result is ", c);
---
+++ Season-01/07-Sync-Async/sync.js
@@ -0,0 +1,10 @@
+let a = 85148;
+let b = 21885;
+
+function multiply(a, b) {
+    return a * b;
+}
+
+let c = multiply(a, b);
+
+console.log("Result is ", c);
